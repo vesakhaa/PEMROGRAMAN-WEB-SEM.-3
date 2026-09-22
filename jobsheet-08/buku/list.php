220c659d7e4c725ded23ffaa8be0a6c7d140d383
vesakhaa <anandavesakha2905@gmail.com>
2026-09-22 16:09:30 +0700

    [JOBSHEET 8] Menambahkan kolom baru 'tanggal_ditambahkan'

--- jobsheet-08/buku/list.php
+++ jobsheet-08/buku/list.php
@@ -28,6 +28,7 @@ $daftarBuku = $pdo->query("SELECT * FROM buku ORDER BY id DESC")->fetchAll(PDO::
                         <th>Pengarang</th>
                         <th>Tahun</th>
                         <th>Stok</th>
+                        <th>Tanggal Ditambahkan</th>
                         <th>Aksi</th>
                     </tr>
                 </thead>
@@ -43,6 +44,7 @@ $daftarBuku = $pdo->query("SELECT * FROM buku ORDER BY id DESC")->fetchAll(PDO::
                             <td><?php echo $buku['pengarang']; ?></td>
                             <td><?php echo $buku['tahun']; ?></td>
                             <td><?php echo $buku['stok']; ?></td>
+                            <td><?php echo $buku['tanggal_ditambahkan']; ?></td>
                             <td>
                                 <button type="button">Edit</button>
                                 <button type="button" class="btn-hapus">Hapus</button>
