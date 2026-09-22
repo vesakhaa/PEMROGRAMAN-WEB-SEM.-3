05d6a807f126235d23bf0a63f30de65c6bd2cc36
vesakhaa <anandavesakha2905@gmail.com>
2026-09-22 13:18:11 +0700

    [JOBSHEET 7] Menambahkan Validasi ISBN

--- jobsheet-07/buku/proses_tambah.php
+++ jobsheet-07/buku/proses_tambah.php
@@ -23,6 +23,12 @@ if (!is_numeric($tahun) || $tahun < 1900 || $tahun > 2026) {
 if (!is_numeric($stok) || $stok < 0) {
     $errors[] = "Stok tidak boleh negatif.";
 }
+if (!empty($isbn) && !preg_match('/^[0-9\-]+$/', $isbn)) {
+    $_SESSION['flash_message'] = "Format ISBN tidak valid! Hanya boleh berisi angka dan tanda hubung.";
+    $_SESSION['flash_type'] = "danger"; 
+    header("Location: tambah.php");
+    exit;
+}
 
 if (!empty($errors)) {
     $_SESSION['flash'] = ['type' => 'error', 'pesan' => implode(' ', $errors)];
